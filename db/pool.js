613281d40e3f0b2fd8205b00a9c81c7e7f53ab94
Christina <184446177+ckim53@users.noreply.github.com>
2025-08-11 17:21:54 -0700

    Add basic styling

--- db/pool.js
+++ db/pool.js
@@ -2,11 +2,8 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-	user: process.env.DB_USER,
-	host: 'localhost',
-	database: process.env.DATABASE,
-	password: process.env.DB_PASSWORD,
-	port: 5432,
+	connectionString: process.env.DATABASE_URL,
+	ssl: { rejectUnauthorized: false }, // for Railway's SSL
 });
 
 module.exports = pool;
